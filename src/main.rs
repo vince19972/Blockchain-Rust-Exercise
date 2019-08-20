8d6e08e43c5287e74422cbbc9aab6679155c1c11
vince19972 <vince19972@gmail.com>
2019-08-20 14:39:10 -0400

    add hashable trait

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,16 @@
+use blockchainlib::*;
+
 fn main() {
-    println!("Hello, blockchain!");
+    let block_str = String::from("Genesis block");
+    let mut block = Block::new(0, 0, vec![0; 32], 0, block_str);
+
+    println!("{:?}", &block);
+
+    let h = block.hash();
+
+    println!("{:?}", &h);
+
+    block.hash = h;
+
+    println!("{:?}", &block);
 }
