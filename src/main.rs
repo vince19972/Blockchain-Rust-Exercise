44af436416e9e92c95fe95a39ba8918cc786cc17
vince19972 <vince19972@gmail.com>
2019-08-22 11:23:18 -0400

    add simple mining function

--- src/main.rs
+++ src/main.rs
@@ -2,15 +2,18 @@ use blockchainlib::*;
 
 fn main() {
     let block_str = String::from("Genesis block");
-    let mut block = Block::new(0, 0, vec![0; 32], 0, block_str);
+    let mut block = Block::new(
+        0,
+        0,
+        vec![0; 32],
+        0,
+        block_str,
+        0x0000ffffffffffffffffffffffffffff,
+    );
 
     println!("{:?}", &block);
 
-    let h = block.hash();
-
-    println!("{:?}", &h);
-
-    block.hash = h;
+    block.mine();
 
     println!("{:?}", &block);
 }
